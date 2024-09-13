5defb4b41e0114b522c8d4fb4dcd044387f31462
Akos07 <akosfodor10@gmail.com>
2024-09-13 09:02:56 +0200

    first element

---
+++ function.js
@@ -0,0 +1,7 @@
+function getFirstElement (arr)
+{
+    const numbers = [1, 2, 3];
+    const words = ["apple", "banana", "cherry"];
+    console.log(`First number: ${getFirstElement(numbers[0])}`);
+    console.log(`First word: ${getFirstElement(words[0])}`);
+}
